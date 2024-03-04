471caa39ad2a38014289e567f9715edd44f5c565
techspence <alessisb@gmail.com>
2024-03-03 19:56:16 -0800

    v0.1 first draft

---
+++ Invoke-ADeleginator.ps1
@@ -0,0 +1,147 @@
+﻿# ADeleginator
+function Invoke-ADeleginator ($PathToADeleg) {
+    # Create ADeleg csv or json report in the current directory
+    function Create-ADelegReport ($PathToADeleg, $ReportName){
+        try {
+            & $PathToADeleg --csv "ADelegReport_$(Get-Date -Format ddMMyyyy).csv"
+        } catch {}
+    }
+
+    # find insecure trustee delegations
+    function Find-InsecureTrusteeDelegations ($ADelegReport, $UnsafeTrustees, $UnsafeDelegations){
+        foreach ($Entry in $ADelegReport) {
+            if ($Entry.Trustee -match $UnsafeTrustees -and $Entry.Category -match "Allow" `
+                -and $Entry.Details -match $UnsafeDelegations) {
+                $InsecureTrusteeDelegations = [pscustomobject]@{
+                    Trustee = $Entry.Trustee
+                    TrusteeType = $Entry.'Trustee Type'
+                    Resource = $Entry.Resource
+                    Category = $Entry.Category
+                    Delegations = $Entry.Details
+                }
+                $InsecureTrusteeDelegations
+            }
+        }
+    }
+
+    # find insecure resource delegations
+    function Find-InsecureResourceDelegations ($ADelegReport, $UnsafeTrustees, $Tier0Resources, $UnsafeDelegations){
+        foreach ($Entry in $ADelegReport) {
+            if ($Entry.Trustee -match $UnsafeTrustees  -and $Entry.Resource -match $Tier0Resources `
+                -and $Entry.Category -match "Allow" -and $Entry.Details -match $UnsafeDelegations) {
+                $InsecureResourceDelegations = [pscustomobject]@{
+                    Trustee = $Entry.Trustee
+                    TrusteeType = $Entry.'Trustee Type'
+                    Resource = $Entry.Resource
+                    Category = $Entry.Category
+                    Delegations = $Entry.Details
+                }
+                $InsecureResourceDelegations
+            }
+        }
+    }
+
+    $UnsafeTrustees = 'Domain Users|Authenticated Users|Everyone'
+    $Tier0Resources = 'Account Operators|Administrator|Administrators|AdminSDHolder|Backup Operators|Cryptographic Operators|Distributed COM Users|Domain Admins|Domain Controllers|Domain Controllers (OU)|Domain root object|DnsAdmins|Enterprise Admins|GPO linked to Tier Zero container|krbtgt|Print Operators|RODC computer object|Schema Admins|Server Operators|Users (container)'
+    $UnsafeDelegations = 'owns|write all properties|create child objects|delete child objects|Change the owner|add/delete delegations|delete'
+
+    $PathToADeleg = '.\ADeleg.exe'
+    $ReportName = "ADelegReport_$(Get-Date -Format ddMMyyyy).csv"
+
+    #ASCII!
+    Write-Host @"
+____ ___  ____ _    ____ ____ _ _  _ ____ ___ ____ ____ 
+|__| |  \ |___ |    |___ | __ | |\ | |__|  |  |  | |__/ 
+|  | |__/ |___ |___ |___ |__] | | \| |  |  |  |__| |  \ 
+                                                        
+by: Spencer Alessi @techspence                     v0.1
+
+          Go, go ADeleginator!
+
+              .'|
+             |  |  _ _
+             |  | (_X_)
+             |  |   |
+              ``.|_.-"-._
+                |.-"""-.|
+               _;.-"""-.;_
+           _.-' _..-.-.._ '-._
+          ';--.-(_o_I_o_)-.--;'
+           ``. | |  | |  | | .``
+             ``-\|  | |  |/-'
+                |  | |  |
+                |  \_/  |
+             _.'; ._._. ;'._
+        _.-'``; | \  -  / | ;'-.
+      .' :  /  |  |   |  |  \  '.
+     /   : /__ \  \___/  / __\ : ``.
+    /    |   /  '._/_\_.'  \   :   ``\
+   /     .  ``---;"""""'-----``  .     \
+  /      |      |()    ()      |      \
+ /      /|      |              |\      \
+/      / |      |()    ()      | \      \
+|         |
+\     \  | ][     |   |    ][  |  /     /
+ \     \ ;=""====='"""'====""==; /     /
+  |/``\  \/      |()    ()      \/  /``\|
+   |_/.-';      |              |``-.\_|
+     /   |      ;              :   \
+     |__.|      |              |.__|
+         ;      |              |
+         |      :              ;
+         |      :              |
+         ;      |              |
+         ;      |              ;
+         |      :              |
+         |      |              ;
+         |      |              ;
+         '-._   ;           _.-'
+             ``;"--.....--";``
+              |    | |    |
+              |    | |    |
+              |    | |    |
+              T----T T----T
+         _..._L____J L____J _..._
+       .`` "-. ``%   | |    %`` .-" ``.
+      /      \    .: :.     /      \
+      '-..___|_..=:`` ``-:=.._|___..-'
+diddle by VK
+
+"@
+
+    if (Get-Item $PathToADeleg -ErrorAction SilentlyContinue) {
+        #continue
+    } else {
+        Write-Warning "ADeleg not found in the current directory. Download and place ADeleg.exe in the same folder as this script, then run ADeleginator again."
+        Write-Warning "You can download ADeleg from here: https://github.com/mtth-bfft/adeleg/releases"
+        break;
+    }
+
+    Write-Host "[i] Running ADeleg and creating $ReportName"
+
+    Create-ADelegReport -PathToADeleg $PathToADeleg -ReportName $ReportName
+
+    $ADelegReport = Import-Csv -Path $ReportName
+
+    Write-Host "[i] Checking for insecure trustee/resource delegations..."
+
+    $InsecureTrusteeDelegations = Find-InsecureTrusteeDelegations -ADelegReport $ADelegReport -UnsafeTrustees $UnsafeTrustees -UnsafeDelegations $UnsafeDelegations
+
+    $InsecureResourceDelegations = Find-InsecureResourceDelegations -ADelegReport $ADelegReport -UnsafeTrustees $UnsafeTrustees -Tier0Resources $Tier0Resources -UnsafeDelegations $UnsafeDelegations
+
+    if ($InsecureTrusteeDelegations) {
+        Write-Host "[!] Insecure trustee delegations found. Exporting report: ADeleg_InsecureTrusteeDelegationReport_$(Get-Date -Format ddMMyyyy).csv" -ForegroundColor Red
+        $InsecureTrusteeDelegations | Export-Csv ADeleg_InsecureTrusteeDelegationReport_$(Get-Date -Format ddMMyyyy).csv -NoTypeInformation
+    } else {
+        Write-Host "[+] No insecure trustee delegations found. Eureka!" -ForegroundColor Green
+    }
+
+    if ($InsecureResourceDelegations) {
+        Write-Host "[!] Insecure resource delegations found. Exporting report: ADeleg_InsecureResourceDelegationReport_$(Get-Date -Format ddMMyyyy).csv" -ForegroundColor Red
+        $InsecureResourceDelegations | Export-Csv ADeleg_InsecureResourceDelegationReport_$(Get-Date -Format ddMMyyyy).csv -NoTypeInformation
+    } else {
+        Write-Host "[+] No insecure resource delegations found. Eureka!" -ForegroundColor Green
+    }
+
+    Write-Host "`n`nThank you for using ADeleginator. Godspeed! :O)`n"
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ADeleginator
 A tool to find insecure trustee and resource delegations in Active Directory
+
+# How to run
+1. Copy Invoke-ADeleginator.ps1 to your system or git clone https://github.com/techspence/ADeleginator
+2. Open PowerShell, navigate to the location of Invoke-ADeleginator.ps1 and dot source it using: . .\Invoke-ADeleginator.ps1
+4. Run with: Invoke-ADeleginator
